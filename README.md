375f7bdff5b45529cdc876b70b9643d5c826e6e9
iNCOHESiONS <incohesions@gmail.com>
2026-07-07 20:08:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prevent Villager Hitting
+
+A client-side, single mixin mod that prevents the player from directly hitting villagers.
---
+++ src/main/java/io/incohesions/prevent_villager_hitting/mixin/VillagerMixin.java
@@ -0,0 +1,20 @@
+package io.incohesions.prevent_villager_hitting.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.npc.villager.Villager;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Villager.class)
+public abstract class VillagerMixin extends Entity {
+
+    public VillagerMixin(EntityType<?> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
+    public boolean isAttackable() {
+        return false;
+    }
+}
